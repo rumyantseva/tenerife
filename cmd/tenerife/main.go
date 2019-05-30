c2d2928dc7373b67e45cc4496181875577ca0926
Elena Grahovac <elena@grahovac.me>
2019-05-30 11:36:28 +0100

    Add signals

--- cmd/tenerife/main.go
+++ cmd/tenerife/main.go
@@ -1,15 +1,17 @@
 package main
 
 import (
-	"github.com/rumyantseva/tenerife/internal/diagnostics"
 	"net"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/gorilla/mux"
 	"github.com/sirupsen/logrus"
 
 	"github.com/rumyantseva/tenerife/internal/application"
+	"github.com/rumyantseva/tenerife/internal/diagnostics"
 )
 
 func main() {
@@ -29,14 +31,32 @@ func main() {
 	r.HandleFunc("/healthz", diagnostics.LivenessHandler(logger))
 	r.HandleFunc("/readyz", diagnostics.ReadinessHandler(logger))
 
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, os.Interrupt, syscall.SIGTERM)
+
+	shutdown := make(chan error, 1)
 
 	server := http.Server{
 		Addr: net.JoinHostPort("", port),
 		Handler: r,
 	}
 
-	err := server.ListenAndServe()
-	if err != nil {
-		logger.Error("Server stopped with an error: %v", err)
+	go func() {
+		err := server.ListenAndServe()
+		shutdown <- err
+	}()
+
+	select {
+	case killSignal := <-interrupt:
+		switch killSignal {
+		case os.Interrupt:
+			logger.Print("Got SIGINT...")
+		case syscall.SIGTERM:
+			logger.Print("Got SIGTERM...")
+		}
+	case <-shutdown:
+		logger.Printf("Got an error...")
 	}
+
+	//server.Shutdown()
 }
