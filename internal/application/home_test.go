14e52b20496f6ef1ce00a87b9a6dfa2d062aa495
Elena Grahovac <elena@grahovac.me>
2019-05-30 16:01:10 +0100

    Fix tests

--- internal/application/home_test.go
+++ internal/application/home_test.go
@@ -1,17 +1,21 @@
 package application
 
 import (
+	"github.com/sirupsen/logrus/hooks/test"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 )
 
 func TestHomeHandler(t *testing.T) {
+	logger, _ := test.NewNullLogger()
+
 	req := httptest.NewRequest(
 		"GET", "http://example.com/foo", nil,
 		)
 	w := httptest.NewRecorder()
-	HomeHandler(w, req)
+	handler := HomeHandler(logger)
+	handler(w, req)
 
 	if http.StatusOK != w.Code {
 		t.Errorf("have %+v, want %+v", w.Code, http.StatusOK)
