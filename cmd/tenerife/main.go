dd75d2e648d3dc42c88be333964f775d5cd44ef9
Elena Grahovac <elena@grahovac.me>
2019-05-30 09:52:07 +0100

    Add a dependency: router

--- cmd/tenerife/main.go
+++ cmd/tenerife/main.go
@@ -4,13 +4,18 @@ import (
 	"net"
 	"net/http"
 	"os"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
 	port := os.Getenv("PORT")
 
+	r := mux.NewRouter()
+
 	server := http.Server{
 		Addr: net.JoinHostPort("", port),
+		Handler: r,
 	}
 
 	server.ListenAndServe()
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/rumyantseva/tenerife
+
+go 1.12
+
+require github.com/gorilla/mux v1.7.2
